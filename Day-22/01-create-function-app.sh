a78834140877b4e47540fa40febca103c02ba35e
Senthilkrishnan Nandhagopal <nsenthilkrishnan@gmail.com>
2025-07-05 08:14:00 +0530

    Update 01-create-function-app.sh

--- Day-22/01-create-function-app.sh
+++ Day-22/01-create-function-app.sh
@@ -3,10 +3,10 @@
 # Variable block
 let "randomIdentifier=$RANDOM*$RANDOM"
 location="eastus"
-resourceGroup="abhi-azure-functions-rg-$randomIdentifier"
+resourceGroup="senthil-azure-functions-rg-$randomIdentifier"
 tag="create-function-app-consumption-python"
-storage="abhi$randomIdentifier"
-functionApp="abhi-serverless-python-function-$randomIdentifier"
+storage="senthilkrishnan$randomIdentifier"
+functionApp="krishnan-serverless-python-function-$randomIdentifier"
 skuStorage="Standard_LRS"
 functionsVersion="4"
 pythonVersion="3.9" #Allowed values: 3.7, 3.8, and 3.9
